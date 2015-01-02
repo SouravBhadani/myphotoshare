e6c54c6e75bc17bedd5ce509cfee79d519c2ab9e
souravbhadani <souravbhadani94@gmail.com>
2015-01-02 20:05:38 +0000

    confiqure to dropbox

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,8 +1,8 @@
 class Pin < ActiveRecord::Base
     belongs_to :user
-    has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" },
-                    :storage => :dropbox,
-                    :dropbox_credentials => Rails.root.join("config/dropbox.yml")
+    has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }#,
+                   # :storage => :dropbox,
+                  #  :dropbox_credentials => Rails.root.join("config/dropbox.yml")
                    
     validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
