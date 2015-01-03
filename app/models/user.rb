ddbda8d13aafc633712c089b82c0522683b94aff
souravbhadani <souravbhadani94@gmail.com>
2015-01-03 22:05:27 +0000

    facebook callback issue fixed

--- app/models/user.rb
+++ app/models/user.rb
@@ -39,7 +39,7 @@ class User < ActiveRecord::Base
       # Create the user if it's a new registration
       if user.nil?
         user = User.new(
-          name: auth.extra.raw_info.name,
+          first_name: auth.extra.raw_info.name,
           #username: auth.info.nickname || auth.uid,
           email: email ? email : "#{TEMP_EMAIL_PREFIX}-#{auth.uid}-#{auth.provider}.com",
           password: Devise.friendly_token[0,20]
