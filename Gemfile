86789768dc7e11a5a9e21fdc5b12d3f4a9d51b8a
souravbhadani <souravbhadani94@gmail.com>
2015-01-04 14:24:34 +0000

    facebook login

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'https://rubygems.org'
 gem 'rails', '4.1.6'
 group :development, :test do
  gem 'sqlite3','1.3.9'
+ gem 'pry'
 end
 group :production do
     gem 'pg',     '0.17.1'
--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,7 +1,7 @@
 class PinsController < ApplicationController
- # before_action :set_pin, only: [:show, :edit, :update, :destroy]
-#  before_action :authenticate_user!, except: [:show, :index]
- # before_action :correct_user, only: [:edit, :update, :destroy]
+  before_action :set_pin, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:show, :index]
+  before_action :correct_user, only: [:edit, :update, :destroy]
 
   respond_to :html
 
