94b3bf5b088755214ad810a4d28e1f4e395fb1b7
souravbhadani <souravbhadani94@gmail.com>
2015-01-07 18:17:43 +0000

    facebook login

---
+++ .c9/metadata/preview-https:/workplace-sourav-bhadani.c9.io
@@ -0,0 +1,1 @@
+{"filter":false,"title":"[P] https://workplace-sourav-bhadani.c9.io","undoManager":{"mark":0,"position":-1,"stack":[]},"preview":{"path":"https://workplace-sourav-bhadani.c9.io","previewer":"preview.browser"},"timestamp":1420654497501,"hash":"da39a3ee5e6b4b0d3255bfef95601890afd80709"}
--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,6 +1,6 @@
 class Pin < ActiveRecord::Base
     belongs_to :user
-    has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>"  },
+    has_attached_file :image, :styles => { :medium => "240x300>", :thumb => "50x50>", :normal => "420x680>"  },
                     :storage => :dropbox,
                     :dropbox_credentials => Rails.root.join("config/dropbox.yml")
                     
