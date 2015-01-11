30c6412df2a4effe2054715d1efaf9098cf7a8bc
souravbhadani <souravbhadani94@gmail.com>
2015-01-11 10:04:01 +0000

    google login

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -15,7 +15,7 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     }
   end
 
-  [:twitter, :facebook, :linked_in].each do |provider|
+  [:twitter, :facebook, :linked_in, :google_oauth2].each do |provider|
     provides_callback_for provider
   end
 
