7393cbf78a66639378aad1a39dc574cf24e3edc4
souravbhadani <souravbhadani94@gmail.com>
2015-08-15 16:34:35 +0000

    added slug to prettylink

--- app/models/prettylink.rb
+++ app/models/prettylink.rb
@@ -1,7 +1,7 @@
 class Prettylink < ActiveRecord::Base
     belongs_to :user
-    
     extend FriendlyId
-    friendly_id :slug_candidates, use: :title
+    friendly_id :title, use: :slugged
+    # friendly_id :slug_candidates, use: :title
     
 end
